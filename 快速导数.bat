7154f970ad8dc509bcc2235277ff980b9e2352d4
frank <franka907@126.com>
2017-04-08 13:23:19 +0800

    1.4版本发布

--- 快速导数.bat
+++ 快速导数.bat
@@ -1,24 +1,41 @@
 ::脚本功能：Oracle快速数据导入与导出
 ::脚本编写：四方精创 欧林海
 ::版本历史：
+::          2016-12-26 Ver1.4
+::                       添加直接按照数据库表清单导出数据功能
+::                       添加数据库端口配置项
+::                       添加导出数据字段分隔符设置
+::                       添加导出数据记录分隔符设置
 ::          2016-12-24 Ver1.3
-::                     合并导入与导出功能，并添加菜单操作（支持鼠标点击）
-::                     添加临时切换数据源功能
-::                     添加目录清理功能
+::                       合并导入与导出功能，并添加菜单操作（支持鼠标点击）
+::                       添加临时切换数据库功能
+::                       添加目录清理功能
 ::          2016-12-23 Ver1.2
-::                     添加数据导入功能，支持自动从csv文件中生成控制文件
+::                       添加数据导入功能，支持自动从csv文件中生成控制文件
 ::          2016-12-02 Ver1.1 
-::                     添加数据库配置及导出格式、数据库字符集的配置
-::                     添加数据库连接测试
+::                       添加数据库配置及导出格式、数据库字符集的配置
+::                       添加数据库连接测试
 ::          2016-11-03 Ver1.0
-::                     初始版本，能实现批量数据导出功能
+::                       初始版本，能实现批量数据导出功能
 @echo off
-title Oracle导数命令行工具 Ver1.3 —by 欧林海
+title Oracle快速导数工具 Ver1.4 —by 欧林海
 ::开启变量延迟
 setlocal EnableDelayedExpansion
+::设置32位系统或64位系统执行标识
+if /i "%PROCESSOR_IDENTIFIER:~0,3%"=="x86" (
+    set "WIN_BIT=Win32" 
+    set "ONLY_X64=REM"
+) else (
+    set "WIN_BIT=Win64" 
+    set "ONLY_X86=REM"
+)
+
 ::切换字符编码
-chcp 437>nul
-graftabl 936>nul
+%ONLY_X64% chcp 936>nul
+%ONLY_X86% chcp 437>nul
+%ONLY_X86% graftabl 936>nul
+::调整字体颜色
+color 0a
 
 ::设置参数
 set CUR_DIR=%~dp0
@@ -30,21 +47,38 @@ set LOG_DIR=log
 set DB_SETTING=数据库设置.txt
 set DB_SETTING_TMP=%BIN_DIR%\dataSource.cfg
 set DB_TEST_SQL=%BIN_DIR%\test_conn.sql
-set DB_TEST_RES=%BIN_DIR%\test_conn.log
+set DB_TEST_RES=%LOG_DIR%\test_conn_res.log
+set DB_TEST_INFO=%LOG_DIR%\test_conn_info.log
 set path=%path%;%CUR_DIR%\%BIN_DIR%
+set TAB_LIST=%SQL_DIR%\table_list.txt
+set CHOICE_TIPS=请选择菜单
+%ONLY_X86% set CHOICE_TIPS=%CHOICE_TIPS%(支持鼠标点击)
+
+::执行环境检查
+::检查Oracle命令行工具是否可用
+for %%a in ( sqlplus.exe sqlldr.exe ) do (
+    where %%a 1>nul 2>nul || ( echo Oracle命令行 %%a 未找到，请确认本机是否安装了带命令行工具的Oracle. && goto end )
+)
+::检查BIN目录中的可执行文件是否缺失
+for %%a in ( sqluldr2.exe szboc_decrypt.exe head.exe choix.com ) do (
+    if not exist %BIN_DIR%\%%a echo 缺少程序必需的文件: %BIN_DIR%\%%a && goto end
+)
+
+::目录初始化
+for %%a in ( %SQL_DIR% %CTL_DIR% %DATA_DIR% %LOG_DIR% ) do if not exist %%a md %%a
 
 ::获取数据库配置选项
 type %DB_SETTING% | findstr "^dataSource">%DB_SETTING_TMP%
-for /f "delims="  %%a in ( %DB_SETTING_TMP% ) do if "%%a" neq "" set "%%a"
+for /f "delims= "  %%a in ( %DB_SETTING_TMP% ) do if "x%%a" neq "x" set "%%a"
 
-if not defined dataSource ( echo dataSource未配置 && goto :configError )
+if not defined dataSource ( echo dataSource未配置 && goto configError )
 
 :setDataSource
-echo =========数据库配置:%dataSource%=========
+echo ==========数据库配置:%dataSource%==========
 
 ::获得配置选项字符串长度
 set /a dataSourceLen=0
-for /l %%i in (0,1,100) do if "!dataSource:~%%i,1!" == "" set /a dataSourceLen=%%i && goto :endfor2
+for /l %%i in (0,1,100) do if "x!dataSource:~%%i,1!" == "x" set /a dataSourceLen=%%i && goto endfor2
 :endfor2
 
 findstr "^%dataSource%" %DB_SETTING%>%DB_SETTING_TMP%
@@ -58,36 +92,36 @@ for /l %%i in (0,1,%fillStrLen%) do set fillStr=!fillStr!=
 echo %fillStr%
 
 ::检查配置项是否完整
-for %%a in ( dbName exportType dbcharset dbUser dbEncPasswd dbIp dbService ) do (
+for %%a in ( dbName exportType fieldSeperator recordSeperator dbCharset dbUser dbEncPasswd dbHostIp dbHostPort dbService ) do (
     if not defined %%a echo %%a未配置，请检查！ && set checkEnvFlag=fail
 )
-if defined checkEnvFlag goto :configError
+if defined checkEnvFlag goto configError
 
 ::设置数据库连接串
-for /f "delims=" %%i in ( '%BIN_DIR%\szboc_decrypt "%dbEncPasswd%"' ) do set dbEnv=%dbUser%/%%i@%dbIp%/%dbService%
+for /f %%i in ( '%BIN_DIR%\szboc_decrypt "%dbEncPasswd%"' ) do (
+    if "x%%i" == "x#ERROR:" echo 数据库密码无法解析 && goto configError
+    set dbEnv=%dbUser%/%%i@%dbHostIp%:%dbHostPort%/%dbService%
+)
 
 ::测试数据库连接
 echo 正在测试数据库连接...
 if exist %DB_TEST_RES% del %DB_TEST_RES%
-sqlplus -L -S %dbEnv% @%DB_TEST_SQL% %DB_TEST_RES%
-if not exist %DB_TEST_RES% goto :connDatabaseError
-if exist %DB_TEST_RES% findstr "success" %DB_TEST_RES% >nul || goto :connDatabaseError
-echo 数据库连接成功
-echo.
+sqlplus -L -S %dbEnv% @%DB_TEST_SQL% %DB_TEST_RES%>%DB_TEST_INFO%
+findstr "success" %DB_TEST_RES% 1>nul 2>nul && echo 数据库连接成功 || goto connDatabaseError
 
 :showMenu
 echo.
-echo (1) 数据导入        
-echo (2) 数据导出
-echo (3) 切换数据源
-echo (4) 清理目录
-echo (5) 退出程序
-echo --------------
-echo 请选择菜单
-choix /c:12345 /M( /N 
+echo   (1) 导入数据
+echo   (2) 导出数据
+echo   (3) 切换数据库
+echo   (4) 清理目录
+echo   (5) 退出程序
+echo ------------------------
+%ONLY_X64% choice /c:12345 /n /m "%CHOICE_TIPS%"
+%ONLY_X86% choix /c:12345 /n /m( "%CHOICE_TIPS%"
 
-for %%a in (1 2 3 4 5) do if errorlevel %%a set item=%%a
-echo 您选择了%item%
+set item=%errorlevel%
+%ONLY_X86% echo 您选择了%item%
 if %item% equ 1 goto importData
 if %item% equ 2 goto exportData
 if %item% equ 3 goto changeDB
@@ -97,118 +131,167 @@ goto end
 
 :importData
 set /a fileCount=0
-for %%a in ( data\*.csv ) do (
-    set "ctlFile=%CTL_DIR%\%%~na.ctl"
+for %%a in ( data\*.* ) do (
+    set fileName=%%~na
+    set fileExtName=%%~xa
+    set "ctlFile=%CTL_DIR%\!fileName!.ctl"
     set /a fileCount=!fileCount!+1
-    if not exist !ctlFile! (
-        echo 正在生成控制文件: !ctlFile!...
-        echo Load Data>!ctlFile!
-        echo Append>>!ctlFile!
-        echo into table %%~na>>!ctlFile!
-        echo fields terminated  by ^',^'>>!ctlFile!
-        echo OPTIONALLY ENCLOSED BY ^'^"^'>>!ctlFile!
-        echo TRAILING NULLCOLS>>!ctlFile!
-        echo ^(>>!ctlFile!
-        head -n 1 %%a>>!ctlFile!
-        echo ^)>>!ctlFile!
+    if /i "x!fileExtName!" equ "x.csv" (    
+        if not exist !ctlFile! (
+            echo 正在生成控制文件:   !ctlFile!...
+            echo Load Data>!ctlFile!
+            echo Append>>!ctlFile!
+            echo into table %%~na>>!ctlFile!
+            echo fields terminated  by ^',^'>>!ctlFile!
+            echo OPTIONALLY ENCLOSED BY ^'^"^'>>!ctlFile!
+            echo TRAILING NULLCOLS>>!ctlFile!
+            echo ^(>>!ctlFile!
+            head -n 1 %%a>>!ctlFile!
+            echo ^)>>!ctlFile!
+        ) else (
+            echo 已经存在的控制文件: !ctlFile!
+        )
     ) else (
-        echo 检测到 %%~na 对应的控制文件已存在
+        if not exist !ctlFile! (
+            echo 数据文件 %%a 缺少对应的控制文件，将被忽略.
+            set /a fileCount=!fileCount!-1
+        ) else (
+            echo 已经存在的控制文件: !ctlFile!
+        )
     )
 )
 echo.
-echo 数据目录中总共有%fileCount%个文件，是否全部导入数据库?
-echo;* Y-确定
-echo;* N-取消
-choix /c:YN /M- /N
-if errorlevel 2 goto :showMenu
-
-echo 开始导数...
+if %fileCount% equ 0 echo 没有要导入的数据文件 && goto continue
+echo 要将这%fileCount%个文件导入【%dbName%】吗?
+echo; * Y-确定
+echo; * N-取消
+::choix /c:yn /m* /n
+%ONLY_X64% choice /c:YN /n /m
+%ONLY_X86% choix /c:YN /n /m*
+if errorlevel 2 echo 您选择了N && goto showMenu
+%ONLY_X86% echo 您选择了Y
+echo.
+echo 导入数据开始...
 for %%a in ( %DATA_DIR%\*.csv ) do (
     set fileName=%%~na
     echo 正在导入!fileName!...
-    sqlldr %dbEnv% control=%CTL_DIR%\!fileName!.ctl data=%DATA_DIR%\!fileName!.csv bad=%DATA_DIR%\!fileName!.bad log=%LOG_DIR%\!fileName!.log skip=1 rows=20000 silent=ALL direct=TRUE
+    sqlldr %dbEnv% control=%CTL_DIR%\!fileName!.ctl data=%DATA_DIR%\!fileName!.csv bad=%DATA_DIR%\!fileName!.bad log=%LOG_DIR%\!fileName!.log skip=1 rows=200000 silent=HEADER direct=TRUE
     findstr /r "加载成功 没有加载" %LOG_DIR%\!fileName!.log
     echo.
 )
-echo 处理完成. 
+for %%a in ( %DATA_DIR%\*.* ) do (
+    set fileName=%%~na
+    set fileExtName=%%~xa
+    if /i "x!fileExtName!" neq "x.csv" if exist %CTL_DIR%\!fileName!.ctl (
+        echo 正在导入!fileName!...
+        sqlldr %dbEnv% control=%CTL_DIR%\!fileName!.ctl data=%DATA_DIR%\!fileName!!fileExtName! bad=%DATA_DIR%\!fileName!.bad log=%LOG_DIR%\!fileName!.log skip=0 rows=20000 silent=HEADER direct=TRUE
+        findstr /r "加载成功 没有加载" %LOG_DIR%\!fileName!.log
+        echo.
+    )
+)
+echo 导入数据结束. 
 goto continue
 
 :exportData
-echo 开始导数...
-for %%a in ( %SQL_DIR%\*.sql ) do (
-    echo 正在导出%%~na...
-    sqluldr2 user=%dbEnv% sql=%%a file=%DATA_DIR%\%%~na.%exportType% text=%exportType% charset=%dbcharset%
+echo 导出数据开始...
+if exist %TAB_LIST% (
+    echo 执行表清单的数据导出...
+    for /f "eol=# delims=" %%a in ( %TAB_LIST% ) do (
+        if "x%%a" neq "x" (
+            for /f "delims= " %%i in ( "%%a" ) do set dataFileName=%%i
+            echo 正在导出 !dataFileName!...
+            sqluldr2 user=%dbEnv% query="select * from %%a" file=%DATA_DIR%\!dataFileName!.%exportType% text=%exportType% charset=%dbCharset% field="%fieldSeperator%" record="%recordSeperator%" rows=500000
+        )
+    )
 )
-echo 处理完成.
+echo.
+if exist %SQL_DIR%\*.sql echo 执行SQL文件的数据导出...
+for %%a in ( %SQL_DIR%\*.sql ) do (
+    echo 正在导出 %%~na...
+    sqluldr2 user=%dbEnv% sql=%%a file=%DATA_DIR%\%%~na.%exportType% text=%exportType% charset=%dbCharset% field="%fieldSeperator%"  record="%recordSeperator%" rows=500000
+)
+echo 导出数据结束.
 goto continue
 
 :changeDB
-findstr "@dbName" %DB_SETTING%>%DB_SETTING_TMP%
+echo 数据库列表>%DB_SETTING_TMP%
+findstr "@dbName" %DB_SETTING%>>%DB_SETTING_TMP%
 set /a seqNo=0
 set seqNoText=
-:showDbConfig
 echo.
-echo 当前已配置的数据源:
+echo 当前已配置的数据库:
 for /f "delims== tokens=2"  %%a in ( %DB_SETTING_TMP% ) do (
     set /a seqNo=seqNo+1
     set seqNoText=%seqNoText%%seqNo%
-    echo;^(!seqNo!^) %%a 
+    echo;  ^(!seqNo!^) %%a
+    if !seqNo! equ 9 goto endfor4
 )
-echo ------------------
-echo 请选择
-choix /c:123456789 /M( /N 
+:endfor4
+echo ------------------------
+::echo 请选择
+::choix /c:123456789 /m( /n 
+%ONLY_X64% choice /c:123456789 /n /m "请选择"
+%ONLY_X86% choix /c:123456789 /n /m( "请选择"
 set item=%errorlevel%
-if %item% gtr %seqNo% echo 无效的选项,请重新输入 && goto showDbConfig
-set /a item=item-1
+%ONLY_X86% echo 您选择了%item%
+if %item% gtr %seqNo% echo 无效的选项,请重新输入 && goto changeDB
 for /f "delims=@ tokens=1 skip=%item%" %%a in ( %DB_SETTING_TMP% ) do (
     set dataSource=%%a
     goto endfor3
 )
 :endfor3
-echo 数据源配置已切换为%dataSource%
+echo 数据库配置已切换为%dataSource%
 goto setDataSource
 
 :cleanDir
 echo 请选择要清理的目录:
-echo (1) 数据文件目录
-echo (2) 控制文件目录
-echo (3) 日志文件目录
-echo (4) SQL文件目录
-echo (5) 除SQL外的全部目录
-echo (6) 全部目录
-echo ------------------
-echo 请选择
-choix /c:123456 /M( /N 
+echo   (1) 数据文件目录
+echo   (2) 控制文件目录
+echo   (3) 日志文件目录
+echo   (4) SQL文件目录
+echo   (5) 除SQL外的全部目录
+echo   (6) 全部目录
+echo   (7) 取消清理
+echo ------------------------
+::echo 请选择
+::choix /c:1234567 /m( /n 
+%ONLY_X64% choice /c:1234567 /n /m "请选择"
+%ONLY_X86% choix /c:1234567 /n /m( "请选择"
 set item=%errorlevel%
-echo 您选择了%item%
-if %item% equ 1 del /q /f %DATA_DIR%\*.*
-if %item% equ 2 del /q /f %CTL_DIR%\*.*
-if %item% equ 3 del /q /f %LOG_DIR%\*.*
-if %item% equ 4 del /q /f %SQL_DIR%\*.*
-if %item% equ 5 del /q /f %DATA_DIR%\*.* %CTL_DIR%\*.* %LOG_DIR%\*.*
-if %item% equ 6 del /q /f %DATA_DIR%\*.* %CTL_DIR%\*.* %LOG_DIR%\*.* %SQL_DIR%\*.*
-echo 清理完成.
+%ONLY_X86% echo 您选择了%item%
+if %item% equ 1 call :cleanDir %DATA_DIR%\*.*
+if %item% equ 2 call :cleanDir %CTL_DIR%\*.ctl
+if %item% equ 3 call :cleanDir %LOG_DIR%\*.log
+if %item% equ 4 call :cleanDir %SQL_DIR%\*.sql
+if %item% equ 5 for %%a in (%DATA_DIR%\*.* %CTL_DIR%\*.ctl %LOG_DIR%\*.log) do call :cleanDir %%a
+if %item% equ 6 for %%a in (%DATA_DIR%\*.* %CTL_DIR%\*.ctl %LOG_DIR%\*.log %SQL_DIR%\*.sql) do call :cleanDir %%a
+if %item% neq 7 echo 清理完成.
 goto continue
 
+:cleanDir
+if "x%1" neq "x" (
+    if exist "%1" del /q /f /s "%1"
+)
+goto :EOF
 
 :continue
-echo 按任意键返回主菜单
-pause>nul
+%ONLY_X86% choix /c /n /m "点击鼠标或按任意键返回主菜单..."
+%ONLY_X64% echo 按任意键返回主菜单...
+%ONLY_X64% pause>nul
 cls
 goto showMenu
 
 :end
-if exist %DB_TEST_RES% del %DB_TEST_RES%
-echo 按任意键退出...
-pause>nul
+%ONLY_X64% echo 按任意键退出...
+%ONLY_X64% pause>nul
+%ONLY_X86% choix /c /n /t 2 /m "即将关闭程序..."
 exit
 
 :configError
-echo 请修改配置后重试！
-goto continue
+echo 请检查数据库参数配置或切换数据库！
+goto changeDB
 
 :connDatabaseError
-echo.
-echo.
-echo 连接数据库失败，请检查配置！
-goto continue
+head -n 2 %DB_TEST_INFO%
+echo 连接数据库失败，请检查配置或切换数据库！
+goto changeDB
